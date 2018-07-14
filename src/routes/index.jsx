b4bee28a16db3daf0d054ff7dd1db33e9a7df1cc
Varma Bhupatiraju <varmab@gmail.com>
2018-07-14 10:27:24 +0530

    Create index.jsx

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import Dashboard from "layouts/Dashboard.jsx";
+import Pages from "layouts/Pages.jsx";
+
+
+var indexRoutes = [
+   { path: "/pages", name: "Home", component: Pages },
+  { path: "/", name: "Dashboard", component: Dashboard }
+];
+
+export default indexRoutes;
